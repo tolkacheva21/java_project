72e120ea3e61f6c39342faea2c48535fb52f89ab
tolkacheva21 <tolkacheva.yulya05@mail.ru>
2024-09-24 07:57:59 +0400

    done 1.2.1 - 1.2.4

--- src/Human.java
+++ src/Human.java
@@ -1,13 +1,24 @@
 public class Human {
-    String name;
+    Name name;
     int height;
+    Human father;
 
-    public Human(String name, int height) {
+    public Human(Name name, int height, Human father) {
         this.name = name;
         this.height = height;
+        this.father = father;
     }
 
     public String toString(){
+        if (father != null && name.surname == null && name.fatherName != null) {
+            return father.name.surname + name + " height: " + height;
+        }
+        else if (father != null && name.fatherName == null && father.name.name != null && name.surname == null){
+            return father.name.surname + name + " " + father.name.name + "ovich" + " height: " + height;
+        }
+        if (father != null && name.fatherName == null && father.name.name != null){
+            return name + " " + father.name.name + "ovich" + " height: " + height;
+        }
         return name + " height: " + height;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        /*Human man1 = new Human("Cleopatra", 152);
-        Human man2 = new Human("Pushkin", 167);
-        Human man3 = new Human("Vladimir", 189);
-        System.out.println(man1);
-        System.out.println(man2);
-        System.out.println(man3);*/
 
         /*Name name1 = new Name("Cleopatra", null, null);
         Name name2 = new Name("Pushkin", "Alexandr", "Sergeevich");
         Name name3 = new Name("Mayakovskiy", "Vladimir", null);
-        System.out.println(name1);
+        Human man1 = new Human(name1, 152);
+        Human man2 = new Human(name2, 167);
+        Human man3 = new Human(name3, 189);*/
+        /*System.out.println(name1);
         System.out.println(name2);
         System.out.println(name3);*/
+        /*System.out.println(man1);
+        System.out.println(man2);
+        System.out.println(man3);*/
+
+
 
         /*Time time1 = new Time(10);
         Time time2 = new Time(10000);
@@ -28,7 +30,7 @@ public class Main {
         System.out.println(house2);
         System.out.println(house3);*/
 
-        Dot point1 = new Dot(1, 3);
+        /*Dot point1 = new Dot(1, 3);
         Dot point2 = new Dot(23, 8);
         Dot point3 = new Dot(5, 10);
         Dot point4 = new Dot(25, 10);
@@ -39,6 +41,28 @@ public class Main {
         point3.x = 7;
         System.out.println(line1);
         System.out.println(line2);
-        System.out.println(line3);
+        System.out.println(line3);*/
+
+        /*Name name4 = new Name("Chudov", "Ivan", null);
+        Name name5 = new Name("Chudov", "Petr", null);
+        Name name6 = new Name(null, "Boris", null);
+        Human man4 = new Human(name4, 180, null);
+        Human man5 = new Human(name5, 192, man4);
+        Human man6 = new Human(name6, 187, man5);
+        System.out.println(man4);
+        System.out.println(man5);
+        System.out.println(man6);*/
+
+        Department department = new Department("IT");
+        Worker worker1 = new Worker("Petrov");
+        Worker worker2 = new Worker("Kozlov");
+        Worker worker3 = new Worker("Sidorov");
+        worker1.department = department;
+        worker2.department = department;
+        worker3.department = department;
+        department.boss = worker2;
+        System.out.println(worker1);
+        System.out.println(worker2);
+        System.out.println(worker3);
     }
 }
